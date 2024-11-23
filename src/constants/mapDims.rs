4a3c31de361aaf5bd01576d7923375903a635227
Peter <peter.hechler@gmail.com>
2024-11-23 13:18:33 -0500

    create wall drawing func

--- src/constants/mapDims.rs
+++ src/constants/mapDims.rs
@@ -1,2 +1,3 @@
 pub const MAP_DIM_X: usize = 32;
 pub const MAP_DIM_Y: usize = 32;
+pub const CELL_SIZE: usize = 32;
--- src/environment/grid.rs
+++ src/environment/grid.rs
@@ -1,6 +1,8 @@
 
 use crate::*;
-use crate::constants::{self, MAP_DIM_X, MAP_DIM_Y};
+use crate::constants::{self, CELL_SIZE, MAP_DIM_X, MAP_DIM_Y};
+
+
 
 pub fn createBlankGrid() -> Vec<Vec<bool>> {
     let mut grid: Vec<Vec<bool>> = vec![];
@@ -11,4 +13,18 @@ pub fn createBlankGrid() -> Vec<Vec<bool>> {
         }
     }
     return grid;
+}
+
+const WALL_COLOR: u32 = 0x96885dFF;
+
+pub fn drawGrid(grid: Vec<Vec<bool>>) {
+    for y in 0..MAP_DIM_Y {
+        for x in 0..MAP_DIM_X {
+            if grid[y][x] {
+                let xCoord: usize = x * CELL_SIZE;
+                let yCoord: usize = y * CELL_SIZE;
+                rect!(x = xCoord, y = yCoord, w = CELL_SIZE, h = CELL_SIZE, color = WALL_COLOR, border_radius = 0);
+            }
+        }
+    }
 }
